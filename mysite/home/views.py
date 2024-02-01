afc6de7c3abb089c9323a782c1383f4d40b2183f
Vicky Kumar <imvickykumar999@gmail.com>
2024-02-01 21:58:29 +0530

    URL Dispatching

--- mysite/home/views.py
+++ mysite/home/views.py
@@ -3,4 +3,13 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Hello World!')
+    return HttpResponse('Home Page')
+
+def about(request):
+    return HttpResponse('About Page')
+
+def services(request):
+    return HttpResponse('Services Page')
+
+def contact(request):
+    return HttpResponse('Contact Page')
