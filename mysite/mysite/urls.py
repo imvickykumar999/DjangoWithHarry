d625566cd313c87672a7f17eb7b9b81b3428e1ed
Vicky Kumar <imvickykumar999@gmail.com>
2024-02-02 12:47:54 +0530

    django admin text
    https://books.agiliq.com/projects/django-admin-cookbook/en/latest/change_text.html

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+admin.site.site_header = "Vick's Administration"
+admin.site.site_title = "Vick's Admin Portal"
+admin.site.index_title = "Welcome to Vick's Researcher Portal"
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls'))
