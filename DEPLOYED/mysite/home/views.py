41bdcd2fb81a1392473f2ba294cfbf8aec1cde39
Vicky Kumar <imvickykumar999@gmail.com>
2024-02-02 15:27:45 +0530

    deployed
    https://djangowithharry.pythonanywhere.com/

---
+++ DEPLOYED/mysite/home/views.py
@@ -0,0 +1,20 @@
+
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+def index(request):
+    context = {
+        'myname' : 'Vicky',
+        'location' : 'Home Page',
+    }
+    # return HttpResponse('Home Page')
+    return render(request, 'index.html', context)
+
+def about(request):
+    return render(request, 'about.html')
+
+def services(request):
+    return render(request, 'services.html')
+
+def contact(request):
+    return render(request, 'contact.html')
