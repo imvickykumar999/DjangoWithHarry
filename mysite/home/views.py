ec4279a1edd1a6d4cc94fe0590ba4a7d0c719323
Vicky Kumar <imvickykumar999@gmail.com>
2024-02-02 13:51:10 +0530

    video embeded

--- mysite/home/views.py
+++ mysite/home/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 def index(request):
     context = {
-        'myname' : 'Vicky',
+        'myname' : 'Guys',
         'location' : 'Home Page',
     }
     # return HttpResponse('Home Page')
