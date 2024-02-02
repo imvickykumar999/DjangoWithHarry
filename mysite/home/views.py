f88ef392460092970e04556228600425e294d50f
Vicky Kumar <imvickykumar999@gmail.com>
2024-02-02 18:50:00 +0530

    Autoplaying carousels
    https://unsplash.com/developers

--- mysite/home/views.py
+++ mysite/home/views.py
@@ -1,20 +1,34 @@
 
 from django.shortcuts import render, HttpResponse
+import requests, random
 
-# Create your views here.
+def get_news(source, api_key='e1b57251b1b94ed894f3c60d25551eb2'):
+    try:
+        gets = f'https://newsapi.org/v1/articles?source={source}&sortBy=top&apiKey={api_key}'
+        req = requests.get(gets)
+        box = req.json()['articles']
+
+    except:
+        gets = f'https://newsapi.org/v1/articles?source={source}&sortBy=top&apiKey=5f69434d32434ea8bdb16b347f71cca2'
+        req = requests.get(gets)
+        box = req.json()['articles']
+    
+    print(box)
+    return box
+    
+def services(request):
+    source = ['bbc-news', 'cnn', 'the-verge', 'time', 'the-wall-street-journal']
+    source = random.choice(source)
+
+    context = get_news(source)
+    return render(request, 'services.html', {'context' : context})
+
 def index(request):
-    context = {
-        'myname' : 'Guys',
-        'location' : 'Home Page',
-    }
+    return render(request, 'index.html')
     # return HttpResponse('Home Page')
-    return render(request, 'index.html', context)
 
 def about(request):
     return render(request, 'about.html')
-
-def services(request):
-    return render(request, 'services.html')
 
 def contact(request):
     return render(request, 'contact.html')
